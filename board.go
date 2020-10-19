1a4acb613206c94650969962c12e18748375d20d
Joshua Whetton <whetton.josh@gmail.com>
2020-10-19 14:55:56 -0400

    Didn't really need iota. Added godoc comments.

--- board.go
+++ board.go
@@ -4,25 +4,23 @@ import (
 	"fmt"
 )
 
-type Player int
-
-const (
-	BLACK Player = iota + 1
-	WHITE
-)
-
+// Space defines spaces on a Go board.
 type Space struct {
 	occupiedBy Player
 }
 
+// Row is an array of 19 Spaces.
 type Row struct {
 	Spaces [19]Space
 }
 
+// Board is an array of 19 Rows to reflect a full
+// Go board.
 type Board struct {
 	Rows [19]Row
 }
 
+// IsOccupied returns true if no Players occupy the space.
 func (s Space) IsOccupied() bool {
 	return s.occupiedBy > 0
 }
@@ -30,11 +28,11 @@ func (s Space) IsOccupied() bool {
 func (s Space) String() string {
 	if s.IsOccupied() {
 		return fmt.Sprint(s.occupiedBy)
-	} else {
-		return "0"
 	}
+	return "0"
 }
 
+// Show will print the state of the Board.
 func (b Board) Show() {
 	for i := range b.Rows {
 		fmt.Println(b.Rows[i])
---
+++ player.go
@@ -0,0 +1,11 @@
+package main
+
+// Player is either 1 (black) or 2 (white).
+type Player int
+
+const (
+	// BLACK player
+	BLACK Player = 1
+	// WHITE player
+	WHITE Player = 2
+)
