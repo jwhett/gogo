ae9895573c42f162b0ce9da0520e59216b452cd1
Joshua Whetton <whetton.josh@gmail.com>
2020-10-19 14:17:23 -0400

    can't occupy properly

--- board.go
+++ board.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	letters = "ABCDEFGHIJKLMNOPQRS"
 )
 
 // Space defines spaces on a Go board.
@@ -25,6 +31,17 @@ func (s Space) IsOccupied() bool {
 	return s.occupiedBy > 0
 }
 
+// Occupy will fill an unoccupied space with a
+// Player's token. Returns an error if one attempts
+// to play in an occupied Space.
+func (s *Space) Occupy(p Player) error {
+	if s.IsOccupied() {
+		return fmt.Errorf("space is taken")
+	}
+	s.occupiedBy = p
+	return nil
+}
+
 func (s Space) String() string {
 	if s.IsOccupied() {
 		return fmt.Sprint(s.occupiedBy)
@@ -37,4 +54,34 @@ func (b Board) Show() {
 	for i := range b.Rows {
 		fmt.Println(b.Rows[i])
 	}
+}
+
+// GetSpace attempts to return a Space from a given
+// set of coordinates.
+func (b Board) GetSpace(coords string) (*Space, error) {
+	x, y, parseErr := parseCoordinates(coords)
+	if parseErr != nil {
+		return nil, parseErr
+	}
+	row := b.Rows[x]
+	space := row.Spaces[y]
+	return &space, nil
+}
+
+func parseCoordinates(s string) (int, int, error) {
+	// split NXX move into X,Y
+	x := strings.Index(letters, s[:1]) + 1
+	y, err := strconv.Atoi(s[1:])
+	return x, y, err
+}
+
+// PlayMove will attempt to occupy a space
+// with a Player's token from the Board level.
+func (b Board) PlayMove(p Player, m string) error {
+	s, err := b.GetSpace(m)
+	if err != nil {
+		return err
+	}
+	s.Occupy(p)
+	return nil
 }
--- main.go
+++ main.go
@@ -7,13 +7,6 @@ import (
 func main() {
 	var board Board
 	fmt.Printf("Black: %d\nWhite: %d\n", BLACK, WHITE)
+	board.PlayMove(BLACK, "D4")
 	board.Show()
 }
-
-/*
-	letters := [19]string{"A", "B", "C", "D",
-                              "E", "F", "G", "H",
-                              "I", "J", "K", "L",
-			      "M", "N", "O", "P",
-                              "Q", "R", "S"}
-*/
